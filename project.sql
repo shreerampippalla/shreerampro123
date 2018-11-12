a77381da23cf344d01f22dc61337f9b50e157481
shreerampippalla <38501178+shreerampippalla@users.noreply.github.com>
2018-11-12 18:26:23 +0530

    Add files via upload

---
+++ project.sql
@@ -0,0 +1,144 @@
+INSERT INTO TRAINEE VALUES (160700,'Eswar','eswar.adusumalli@gmail.com',9791273799,'Hyderabad',150700,'RDBMS');
+INSERT INTO TRAINEE VALUES (160701,'Surya','surya.adda@gmail.com',7708516126,'Bangalore',150703,'Business Inteligence');
+INSERT INTO TRAINEE VALUES (160702,'Praneet','praneet.k@gmail.com',7708528366,'Chennai',150701,'Javascript');
+INSERT INTO TRAINEE VALUES (160703,'Uday','uday123@gmail.com',9441096107,'Banglore',150702,'JEE');
+INSERT INTO TRAINEE VALUES (160704,'Palani','palani.chinna@gmail.com',9791489109,'Banglore',150700,'RDBMS');
+INSERT INTO TRAINEE VALUES (160705,'Mitra','mitra.darapuneni@gmail.com',9791489276,'Hyderabad',150702,'JEE');
+INSERT INTO TRAINEE VALUES (160706,'priya','priya.ki@gmail.com',9658475264,'Hyderabad',150701,'Javascript');
+INSERT INTO TRAINEE VALUES (160707,'Hari','hari.ippili@gmail.com',9790461626,'Chennai',150702,'JEE');
+INSERT INTO TRAINEE VALUES (160708,'varun','varun.rao@gmail.com',9176437234,'kakinada',150703,'Business Intelligence');
+INSERT INTO TRAINEE VALUES (160709,'satish','satish.rajareddy@gmail.com',8143767601,'Rajahmundry',150700,'RDBMS');
+INSERT INTO TRAINEE VALUES (160710,'Keerthi','keerthi.suresh@gmail.com',8220480572,'Mumbai',150700,'RDBMS');
+INSERT INTO TRAINEE VALUES (160711,'Abhay','abhay.sharma@gmail.com',8309058599,'Ranchi',150701,'Javascript');
+INSERT INTO TRAINEE VALUES (160712,'Dheeraj','dheeraj.gupta@gmail.com',8309074799,'Mumbai',150701,'Javascript');
+INSERT INTO TRAINEE VALUES (160713,'Apoorva','apoorva.singh@gmail.com',8247716928,'pune',150702,'JEE');
+INSERT INTO TRAINEE VALUES (160714,'Snikda','snikda.sharma@gmail.com',9848188613,'Delhi',150702,'JEE');
+INSERT INTO TRAINEE VALUES (160715,'saurabh','saurabh.tiwari@gmail.com',9791489273,'Agra',150700,'RDBMS');
+INSERT INTO TRAINEE VALUES (160716,'vritansh','vritansh.kamal@gmail.com',7708515276,'Pune',150700,'RDBMS');
+INSERT INTO TRAINEE VALUES (160717,'DeepAnand','deep.anand@gmail.com',9848522336,'Hyderabad',150703,'Business Intelligence');
+INSERT INTO TRAINEE VALUES (160718,'Anudeep','anudeep.gupta@gmail.com',9676364516,'kolkata',150701,'Javascript');
+INSERT INTO TRAINEE VALUES (160719,'Ananya','ananya.singh@gmail.com',8974515268,'Banglore',150701,'Javascript');
+INSERT INTO TRAINEE VALUES (160720,'sudha','sudha.singh@gmail.com',7715963857,'Noida',150702,'JEE');
+INSERT INTO TRAINEE VALUES (160721,'Sakshi','sakshi.sinha@gmail.com',7556932744,'Mumbai',150702,'JEE');
+INSERT INTO TRAINEE VALUES (160722,'Bilvika','bilvika.kasturi@gmail.com',6523587912,'Vizag',150703,'Business Intelligence');
+INSERT INTO TRAINEE VALUES (160723,'Moulith','moulith.kumar@gmail.com',8014740704,'Hyderabad',150700,'RDBMS');
+INSERT INTO TRAINEE VALUES (160724,'Vikas','vikas11@gmail.com',9791273791,'Hyderabad',150701,'Javascript');
+INSERT INTO TRAINEE VALUES (160725,'Anil','anilomkar12@gmail.com',9791273798,'Chennai',150700,'RDBMS');
+INSERT INTO TRAINEE VALUES (160726,'Prashant','prashant@gmail.com',9791273790,'Hyderabad',150700,'RDBMS');
+INSERT INTO TRAINEE VALUES (160727,'Mohitha','mohitha11@gmail.com',9791273712,'Hyderabad',150702,'JEE');
+INSERT INTO TRAINEE VALUES (160728,'Lakshmi','lakshmisree@gmail.com',7791273799,'Delhi',150703,'Business Intelligence');
+INSERT INTO TRAINEE VALUES (160729,'Rachana','Rachanaagarwal@gmail.com',8793273799,'Chennai',150703,'Business Intelligence');
+INSERT INTO TRAINEE VALUES (160730,'Avinash','avinashvarma@gmail.com',7791273779,'Hyderabad',150700,'RDBMS');
+INSERT INTO TRAINEE VALUES (160731,'suvarna','suvarna.singh@gmail.com',7215963857,'Noida',150702,'JEE');
+INSERT INTO TRAINEE VALUES (160732,'Shashank','shashank.sharma@gmail.com',7546832744,'Mumbai',150703,'Business Intelligence');
+INSERT INTO TRAINEE VALUES (160733,'Swati','swati.sai@gmail.com',6423586912,'Vizag',150701,'Javascript');
+INSERT INTO TRAINEE VALUES (160734,'Shyam','shyam.kumar@gmail.com',8024740704,'Chennai',150700,'RDBMS');
+INSERT INTO TRAINEE VALUES (160735,'Monisha','monisha11@gmail.com',9001373791,'Hyderabad',150702,'JEE');
+INSERT INTO TRAINEE VALUES (160736,'sudharani','sudharani.singh@gmail.com',7714963857,'Noida',150700,'RDBMS');
+INSERT INTO TRAINEE VALUES (160737,'Sakshit','sakshit.sinha@gmail.com',7556932744,'Mumbai',150702,'JEE');
+INSERT INTO TRAINEE VALUES (160738,'vivek','vivek.kasturi@gmail.com',6323587912,'Bhubhaneshwar',150703,'Business Intelligence');
+INSERT INTO TRAINEE VALUES (160739,'Varshini','Varshini.kumar@gmail.com',8004740704,'nagpur',150701,'Javascript');
+INSERT INTO TRAINEE VALUES (160740,'Sahitya','Sahitya@gmail.com',9781273791,'pune',150701,'Javascript');
+INSERT INTO TRAINEE VALUES (160741,'Sahithi','Sahithi@gmail.com',9781243791,'Noida',150700,'RDBMS');
+INSERT INTO TRAINEE VALUES (160742,'Pavan','pavankumar@gmail.com',9781003791,'Mumbai',150702,'JEE');
+INSERT INTO TRAINEE VALUES (160743,'Nayani','Nayani.pavani@gmail.com',9781233791,'Agra',150701,'Javascript');
+INSERT INTO TRAINEE VALUES (160744,'Revanth','Revanth@gmail.com',9780073700,'Orrissa',150701,'Javascript');
+INSERT INTO TRAINEE VALUES (160745,'Devika','Devika@gmail.com',9000273791,'Mumbai',150701,'Javascript');
+INSERT INTO TRAINEE VALUES (160746,'Sanajana','Sanjana@gmail.com',9781003791,'Mumbai',150702,'JEE');
+INSERT INTO TRAINEE VALUES (160747,'Sajaysharma','Sanajay.sharma@gmail.com',9781270001,'pune',150702,'JEE');
+INSERT INTO TRAINEE VALUES (160748,'Subhodaya','subhodaya@gmail.com',9221273791,'pune',150702,'JEE');
+INSERT INTO TRAINEE VALUES (160749,'Rakul','Rakul.preet@gmail.com',9111273791,'Hyderabad',150700,'RDBMS');
+INSERT INTO TRAINEE VALUES (160750,'Neelesh','neelesh@gmail.com',9733373791,'Hyderabad',150700,'RDBMs');
+INSERT INTO TRAINEE VALUES (160751,'Kajal','kajal.agarwal@gmail.com',9781272291,'Vizag',150700,'RDBMS');
+INSERT INTO TRAINEE VALUES (160752,'Anupama','Anupama.parvataneni@gmail.com',9781173791,'Vizag',150701,'Javascript');
+INSERT INTO TRAINEE VALUES (160753,'Mahesh','mahesh.nani@gmail.com',9781222791,'Mumbai',150703,'Business Intelligence');
+INSERT INTO TRAINEE VALUES (160754,'Arun','arun.divya@gmail.com',9999273791,'Mumbai',150703,'Business Intelligence');
+INSERT INTO TRAINEE VALUES (160755,'Vijay','Vijay@gmail.com',9995773791,'Mumbai',150703,'Business Intelligence');
+INSERT INTO TRAINEE VALUES (160756,'Aruna','aruna@gmail.com',9666273791,'Ranchi',150700,'RDBMS');
+INSERT INTO TRAINEE VALUES (160757,'Vilas','vilas@gmail.com',9999291000,'Ranchi',150700,'RDBMS');
+INSERT INTO TRAINEE VALUES (160758,'Rashmika','Rashmika.mohith@gmail.com',8999273791,'Pune',150701,'Javascript');
+INSERT INTO TRAINEE VALUES (160759,'Sankeerth','Sankeerth.divya@gmail.com',9992273791,'Hyderabad',150701,'Javascript');
+INSERT INTO TRAINEE VALUES (160760,'Niveda','niveda.t@gmail.com',9999200791,'Hyderabad',150701,'Javascript');
+INSERT INTO TRAINEE VALUES (160761,'Ramesh','ramesh.jakkampudi@gmail.com',9798920079,'Banglore',150703,'Business Intelligence');
+INSERT INTO TRAINEE VALUES (160762,'Suresh','suresh.kolliboina@gmail.com',8575698529,'Chennai',150703,'Business Intelligence');
+INSERT INTO TRAINEE VALUES (160763,'Anushka','Anushka.sharma@gmail.com',9676543978,'Mumbai',150703,'Business Intelligence');
+INSERT INTO TRAINEE VALUES (160764,'Shivansh','shivansh.gupta@gmail.com',8576954892,'Kolkata',150702,'JEE');
+INSERT INTO TRAINEE VALUES (160765,'Sayesha','sayesha.sehgal@gmail.com',9988428769,'Delhi',150702,'JEE');
+INSERT INTO TRAINEE VALUES (160766,'Himanshu','himanshu.singh@gmail.com',6541785969,'Banglore',150700,'RDBMS');
+INSERT INTO TRAINEE VALUES (160767,'Shreyansh','shreyansh.kumar@gmail.com',9735442554,'Bhubaneswar',150700,'RDBMS');
+INSERT INTO TRAINEE VALUES (160768,'Namratha','namratha.mahesh@gmail.com',9243576982,'Pune',150701,'Javascript');
+INSERT INTO TRAINEE VALUES (160769,'Suraj','suraj.nagaraj@gmail.com',8572859634,'Hyderabad',150703,'Business Intelligence');
+INSERT INTO TRAINEE VALUES (160770,'Sravan','sravan.shetty@gmail.com',9428956753,'Banglore',150702,'JEE');
+INSERT INTO TRAINEE VALUES (160771,'Sravanthi','sravanthi.sharma@gmail.com',9852367589,'Jharkhand',150701,'Javascript');
+INSERT INTO TRAINEE VALUES (160772,'Sravya','sravya.singh@gmail.com',9763584287,'Noida',150703,'Business Intelligence');
+INSERT INTO TRAINEE VALUES (160773,'Sushanth','sushanth.kumar@gmail.com',8523475964,'Hyderabad',150702,'JEE');
+INSERT INTO TRAINEE VALUES (160774,'Prarthana','prarthana.huparikar@gmail.com',8574298569,'Hyderabad',150700,'RDBMS');
+
+
+INSERT INTO SLOT VALUES(1001,'19SEP_BLR_PSN',TO_DATE('19-SEP-2018', 'DD-MON-YYYY'),TO_DATE('04-DEC-2018', 'DD-MON-YYYY'));
+INSERT INTO SLOT VALUES(1002,'23SEP_BLR_DTP',TO_DATE('23-SEP-2018', 'DD-MON-YYYY'),TO_DATE('14-DEC-2018', 'DD-MON-YYYY'));
+INSERT INTO SLOT VALUES(1003,'17SEP_BLR_STP',TO_DATE('17-SEP-2018', 'DD-MON-YYYY'),TO_DATE('30-NOV-2018', 'DD-MON-YYYY'));
+INSERT INTO SLOT VALUES(1004,'25SEP_BLR_PSN',TO_DATE('25-SEP-2018', 'DD-MON-YYYY'),TO_DATE('18-DEC-2018', 'DD-MON-YYYY'));
+
+INSERT INTO DOMAIN VALUES('RDBMS',150700,TO_DATE('24-SEP-2018', 'DD-MON-YYYY'),TO_DATE('04-DEC-2018', 'DD-MON-YYYY'));
+INSERT INTO DOMAIN VALUES('Javascript',150701,TO_DATE('24-SEP-2018', 'DD-MON-YYYY'),TO_DATE('10-DEC-2018', 'DD-MON-YYYY'));
+INSERT INTO DOMAIN VALUES('JEE',150702,TO_DATE('09-SEP-2018', 'DD-MON-YYYY'),TO_DATE('06-DEC-2018', 'DD-MON-YYYY'));
+INSERT INTO DOMAIN VALUES('Business Intelligence',150703,TO_DATE('24-SEP-2018', 'DD-MON-YYYY'),TO_DATE('17-DEC-2018', 'DD-MON-YYYY'));
+
+INSERT INTO TRAINER VALUES(145743,'Karthikeyan',101,'karthikeyan.sivan@gmail.com',9852369786);
+INSERT INTO TRAINER VALUES(148765,'Ankitha Sakpal',102,'ankitha.sakpal@gmail.com',7528954296);
+INSERT INTO TRAINER VALUES(142358,'Chandrasekhar',103,'chandrasekahr.ravula@gmail.com',9912345689);
+INSERT INTO TRAINER VALUES(148987,'Shubhangi',104,'shubhangi.sharma@gmail.com',8676754595);
+INSERT INTO TRAINER VALUES(154629,'Hari Yadav',105,'hari.yadav@gmail.com',9215545889);
+INSERT INTO TRAINER VALUES(143954,'srinivasan',106,'srinivasan.kumaran@gmail.com',7725856929);
+INSERT INTO TRAINER VALUES(149942,'Radhika',107,'radhika.rajkumar@gmail.com',9941258753);
+INSERT INTO TRAINER VALUES(134254,'Narasimhan',108,'narasimhan.narendar@gmail.com',6587258963);
+INSERT INTO TRAINER VALUES(135793,'Vijaya',109,'vijaya.rajesh@gmail.com',7859658685);
+INSERT INTO TRAINER VALUES(132783,'Sachin',110,'sachin.narendakar@gmail.com',9878526985);
+INSERT INTO TRAINER VALUES(130012,'Rakhee',111,'rakhee.harlalka@gmail.com',7542156255);
+INSERT INTO TRAINER VALUES(150012,'Rajesekhar',112,'rajasekhar.reddy@gmail.com',9632145879);
+
+INSERT INTO MENTOR VALUES(134567,'Devangana','RDBMS','devangana.mandal@gmail.com',7528423639);
+INSERT INTO MENTOR VALUES(139864,'Mangala','Javascript','mangala.gowri@gmail.com',9854712968);
+INSERT INTO MENTOR VALUES(131546,'sachin','JEE','sachin.narendakar@gmail.com',9952853426);
+INSERT INTO MENTOR VALUES(134789,'Srinivasan','Business Intelligence','srinivasan.s@gmail.com',8220480571);
+
+INSERT INTO HR VALUES(139876,'Onkaramurthy',1001,'onkaramurthy.shivanna@gmail.com',7532196485);
+INSERT INTO HR VALUES(138523,'Gowrishankar',1002,'gowri.shankar@gmail.com',9632147859);
+INSERT INTO HR VALUES(133249,'Monica',1003,'monica.rajesh@gmail.com',9741258637);
+INSERT INTO HR VALUES(137654,'Shivani',1004,'shivani.sinha@gmail.com',8525463971);
+
+INSERT INTO MODULE1 VALUES(420,'Core Java','JEE',145743,TO_DATE('24-SEP-2018', 'DD-MON-YYYY'),TO_DATE('14-OCT-2018', 'DD-MON-YYYY'),TO_DATE('14-OCT-2018', 'DD-MON-YYYY'));
+INSERT INTO MODULE1 VALUES(423,'HTML CSS','Javascript',148987,TO_DATE('24-SEP-2018', 'DD-MON-YYYY'),TO_DATE('28-OCT-2018', 'DD-MON-YYYY'),TO_DATE('28-OCT-2018', 'DD-MON-YYYY'));
+INSERT INTO MODULE1 VALUES(426,'ORACLE','Business Intelligence',149942,TO_DATE('24-SEP-2018', 'DD-MON-YYYY'),TO_DATE('13-OCT-2018', 'DD-MON-YYYY'),TO_DATE('13-OCT-2018', 'DD-MON-YYYY'));
+INSERT INTO MODULE1 VALUES(429,'SQL/PLSQL','RDBMS',132783,TO_DATE('24-SEP-2018', 'DD-MON-YYYY'),TO_DATE('09-OCT-2018', 'DD-MON-YYYY'),TO_DATE('09-OCT-2018', 'DD-MON-YYYY'));
+
+INSERT INTO MODULE2 VALUES(421,'Servelts/JSP','JEE',148765,TO_DATE('15-OCT-2018', 'DD-MON-YYYY'),TO_DATE('08-NOV-2018', 'DD-MON-YYYY'),TO_DATE('08-NOV-2018', 'DD-MON-YYYY'));
+INSERT INTO MODULE2 VALUES(424,'Angular JS','Javascript',154629,TO_DATE('29-OCT-2018', 'DD-MON-YYYY'),TO_DATE('16-NOV-2018', 'DD-MON-YYYY'),TO_DATE('16-NOV-2018', 'DD-MON-YYYY'));
+INSERT INTO MODULE2 VALUES(427,'Teradata','Business Intelligence',134254,TO_DATE('14-OCT-2018', 'DD-MON-YYYY'),TO_DATE('08-NOV-2018', 'DD-MON-YYYY'),TO_DATE('08-NOV-2018', 'DD-MON-YYYY'));
+INSERT INTO MODULE2 VALUES(430,'Unix','RDBMS',130012,TO_DATE('10-OCT-2018', 'DD-MON-YYYY'),TO_DATE('13-NOV-2018', 'DD-MON-YYYY'),TO_DATE('13-NOV-2018', 'DD-MON-YYYY'));
+
+INSERT INTO MODULE3 VALUES(422,'Spring','JEE',142358,TO_DATE('09-NOV-2018', 'DD-MON-YYYY'),TO_DATE('06-DEC-2018', 'DD-MON-YYYY'),TO_DATE('06-DEC-2018', 'DD-MON-YYYY'));
+INSERT INTO MODULE3 VALUES(425,'Jquery','Javascript',143954,TO_DATE('17-NOV-2018', 'DD-MON-YYYY'),TO_DATE('10-DEC-2018', 'DD-MON-YYYY'),TO_DATE('10-DEC-2018', 'DD-MON-YYYY'));
+INSERT INTO MODULE3 VALUES(428,'Abinitio','Business Intelligence',135793,TO_DATE('09-NOV-2018', 'DD-MON-YYYY'),TO_DATE('16-DEC-2018', 'DD-MON-YYYY'),TO_DATE('16-DEC-2018', 'DD-MON-YYYY'));
+INSERT INTO MODULE3 VALUES(431,'Mongo DB','RDBMS',150012,TO_DATE('14-NOV-2018', 'DD-MON-YYYY'),TO_DATE('04-DEC-2018', 'DD-MON-YYYY'),TO_DATE('04-DEC-2018', 'DD-MON-YYYY'));
+
+
+INSERT INTO L1 VALUES('RDBMS',TO_DATE('06-DEC-2018', 'DD-MON-YYYY'));
+INSERT INTO L1 VALUES('Javascript',TO_DATE('14-DEC-2018', 'DD-MON-YYYY'));
+INSERT INTO L1 VALUES('JEE',TO_DATE('10-DEC-2018', 'DD-MON-YYYY'));
+INSERT INTO L1 VALUES('Business Intelligence',TO_DATE('19-DEC-2018', 'DD-MON-YYYY'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
